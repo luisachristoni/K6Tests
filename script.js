afbfc5aadae428286b3d4ece1480d9eacfa62409
luisachristoni <luisachristoni@hotmail.com>
2026-02-10 17:52:38 -0300

    Adicionando threshold

--- script.js
+++ script.js
@@ -3,8 +3,13 @@ import { sleep } from "k6";
 import {criarPet, buscarPet} from "./api/petApi.js";
 
 export const options = {
-    vus:2,
-    duration: "10s"
+    vus: 2,
+    duration: "10s",
+    thresholds: {
+        http_req_failed: ["rate<0.01"],     // menos de 1% de erro
+        http_req_duration: ["p(95)<200"],   // 95% < 500ms
+        checks: ["rate>0.99"],              // 99% dos checks passando
+    },
 };
 
 export default function() {
